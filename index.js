0f893479992c498fb9def20da4677f1ee475c1a1
truar <thibault.ruaro@gmail.com>
2018-09-07 17:32:16 +0200

    Last commit. I haven't been really good on this one... I will do better next time

--- index.js
+++ index.js
@@ -11,11 +11,18 @@ mongoose.connect('mongodb://localhost/guests');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
-}))
+}));
 
-// appel la fonction définit par le export dans cats.js
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+	next();
+  });
+
+// appel la fonction définit par le export dans guests.js
 var guests = require('./routes/guest.js')(app)
  
-var server = app.listen(3000, function() {
-	console.log('Server running at http://localhost:3000/');
+var server = app.listen(3001, function() {
+	console.log('Server running at http://localhost:3001/');
 });
